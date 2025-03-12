391c7e78d493d5e81f6e2808d0d351f33b87c006
kduckets <kmditroia@gmail.com>
2025-03-12 13:40:01 -0400

    Update page.tsx

--- app/providers/page.tsx
+++ app/providers/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react"
 import ProviderSearch from "./ProviderSearch"
+import { LoadingState } from "../components/LoadingState"
 
 export default function ProvidersPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingState />}>
       <ProviderSearch />
     </Suspense>
   )
